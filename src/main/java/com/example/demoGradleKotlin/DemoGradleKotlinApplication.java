f0fe305474a28c0aa01bb1817b2dcd1f6c5cb6a1
Hansika Mannapperuma <hansikamannapperuma@gmail.com>
2023-02-17 10:33:43 +0530

    initial commit

---
+++ src/main/java/com/example/demoGradleKotlin/DemoGradleKotlinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoGradleKotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleKotlinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoGradleKotlin/DemoGradleKotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoGradleKotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleKotlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
